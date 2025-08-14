cf66327ca01f3b88a8c68342f04c5b9780ce2d2a
riyaverma2723-eng <riyaverma2723@gmail.com>
2025-08-14 12:55:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function updateClock() {
+    let now = new Date();
+
+    let hours = String(now.getHours()).padStart(2, '0');
+    let minutes = String(now.getMinutes()).padStart(2, '0');
+    let seconds = String(now.getSeconds()).padStart(2, '0');
+
+    document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock(); // Initial call so clock doesn't wait 1 second
